68ac69d212da9279a6dbe690328cefa69473556d
kxyang <kx.yang.apply@outlook.com>
2025-05-18 16:35:28 +0800

    support chinese

--- crates/indexify/src/utils.rs
+++ crates/indexify/src/utils.rs
@@ -6,9 +6,12 @@ use tantivy::{
     Document, Index, IndexReader, IndexWriter, ReloadPolicy, TantivyDocument, TantivyError,
     collector::TopDocs,
     doc,
-    query::QueryParser,
-    schema::{STORED, Schema, TEXT},
+    query::{Query, QueryParser},
+    schema::{IndexRecordOption, STORED, Schema, TEXT, TextFieldIndexing, TextOptions},
+    tokenizer::{NgramTokenizer, SimpleTokenizer, TokenStream, Tokenizer},
 };
+use tantivy_jieba::JiebaTokenizer;
+use whichlang::{Lang, detect_language};
 
 pub static TANTIVY_INDEX: Lazy<TantivyIndex> = Lazy::new(|| {
     tracing::debug!("Initializing tantivy index...");
@@ -26,7 +29,15 @@ impl TantivyIndex {
     pub fn new() -> Result<Self, TantivyError> {
         tracing::debug!("Building initial tantivy index...");
         let mut schema_builder = Schema::builder();
-        schema_builder.add_text_field("name", TEXT | STORED);
+
+        let name_options = TextOptions::default()
+            .set_indexing_options(
+                TextFieldIndexing::default()
+                    .set_tokenizer("mixed")
+                    .set_index_option(IndexRecordOption::WithFreqsAndPositions),
+            )
+            .set_stored();
+        schema_builder.add_text_field("name", name_options);
         schema_builder.add_text_field("path", STORED);
         // schema_builder.add_bytes_field("if_folder", STORED);
         let schema = schema_builder.build();
@@ -36,6 +47,7 @@ impl TantivyIndex {
             Ok(index) => index,
             Err(_) => {
                 if let Err(e) = fs::create_dir_all(&index_path) {
+                    tracing::error!("Failed to create directory at {}: {}", &index_path, e);
                     panic!("Failed to create directory at {}: {}", &index_path, e);
                 }
                 match Index::create_in_dir(&index_path, schema.clone()) {
@@ -49,6 +61,9 @@ impl TantivyIndex {
                 }
             }
         };
+
+        let mixed_tokenizer = MixedTokenizer::new();
+        index.tokenizers().register("mixed", mixed_tokenizer);
 
         tracing::debug!("Creating index writer and reader...");
         let index_writer = index.writer(50_000_000)?;
@@ -82,6 +97,7 @@ impl TantivyIndex {
         let query_parser =
             QueryParser::for_index(&self.index, vec![self.schema.get_field("name").unwrap()]);
         let query = query_parser.parse_query(query)?;
+
         let top_docs = searcher.search(&query, &TopDocs::with_limit(10))?;
         tracing::debug!("Found {} results", top_docs.len());
         for (_score, doc_address) in top_docs {
@@ -102,8 +118,153 @@ impl TantivyIndex {
     }
 }
 
+#[derive(Clone)]
+struct MixedTokenizer {
+    default_tokenizer: SimpleTokenizer,
+    jieba_tokenizer: JiebaTokenizer,
+}
+
+impl MixedTokenizer {
+    fn new() -> Self {
+        MixedTokenizer {
+            default_tokenizer: SimpleTokenizer::default(),
+            jieba_tokenizer: tantivy_jieba::JiebaTokenizer {},
+        }
+    }
+}
+
+impl Tokenizer for MixedTokenizer {
+    type TokenStream<'a> = Box<dyn TokenStream + 'a>;
+
+    fn token_stream<'a>(&'a mut self, text: &'a str) -> Self::TokenStream<'a> {
+        match detect_language(text) {
+            Lang::Cmn => {
+                tracing::debug!("Detected Chinese language");
+                tracing::debug!("Text: {}", text);
+                Box::new(self.jieba_tokenizer.token_stream(text))
+            }
+            _ => Box::new(self.default_tokenizer.token_stream(text)),
+        }
+    }
+}
+
 pub fn get_files(path: &str) -> Result<WalkDirGeneric<((), ())>, jwalk::Error> {
     tracing::debug!("Getting files from {}", path);
     let files = WalkDir::new(path).sort(false).skip_hidden(false);
     Ok(files)
+}
+
+#[cfg(test)]
+mod tests {
+    use jwalk::rayon::vec;
+
+    use super::*;
+
+    #[test]
+    fn chinese_tokenizer() {
+        let path = "./tmp";
+        let _ = fs::remove_dir_all(path);
+        fs::create_dir(path).ok();
+
+        let schema_builder = Schema::builder();
+        let schema = schema_builder.build();
+
+        let index = Index::create_in_dir(&path, schema.clone()).unwrap();
+
+        let mixed_tokenizer: MixedTokenizer = MixedTokenizer::new();
+        index.tokenizers().register("mixed", mixed_tokenizer);
+
+        {
+            let mut tokenizer = index.tokenizers().get("mixed").unwrap();
+            let text = "无人驾驶数据与隐私_何凡（1）.docx";
+            let mut token_stream = tokenizer.token_stream(text);
+
+            println!("建立索引分词结果:");
+            token_stream.process(&mut |token| {
+                println!(
+                    "Token: '{}', 位置: {}, 偏移量: {}-{}",
+                    token.text, token.position, token.offset_from, token.offset_to
+                );
+            });
+        }
+
+        {
+            let mut tokenizer = index.tokenizers().get("mixed").unwrap();
+            let query_text: &'static str = "中国";
+            let mut token_stream = tokenizer.token_stream(query_text);
+
+            println!("查询分词结果:");
+            token_stream.process(&mut |token| {
+                println!(
+                    "Token: '{}', 位置: {}, 偏移量: {}-{}",
+                    token.text, token.position, token.offset_from, token.offset_to
+                );
+            });
+        }
+        let _ = fs::remove_dir_all(path);
+    }
+
+    #[test]
+    fn chinese_doc_tokenizer() {
+        let mut schema_builder = Schema::builder();
+        let text_options = TextOptions::default()
+            .set_indexing_options(
+                TextFieldIndexing::default()
+                    .set_tokenizer("mixed")
+                    .set_index_option(IndexRecordOption::WithFreqsAndPositions),
+            )
+            .set_stored();
+        let name = schema_builder.add_text_field("name", text_options);
+        let schema = schema_builder.build();
+
+        let index = Index::create_in_ram(schema.clone());
+
+        index.tokenizers().register("mixed", MixedTokenizer::new());
+        let texts = vec![
+            "中文的原神.txt",
+            "你好世界.docx",
+            "这是一个测试文档.pdf",
+            "北京欢迎你.md",
+            "今天天气怎么样.txt",
+            "我喜欢学习编程.rs",
+            "人工智能的未来.html",
+        ];
+
+        let mut index_writer = index.writer(50_000_000).unwrap();
+        for text in texts {
+            index_writer
+                .add_document(doc!(
+                    name => text
+                ))
+                .unwrap();
+        }
+        index_writer.commit().unwrap();
+
+        let query_text = "北京";
+
+        println!("\n查询分词结果:");
+        let mut query_tokenizer = index.tokenizers().get("mixed").unwrap();
+        let mut query_token_stream = query_tokenizer.token_stream(query_text);
+        query_token_stream.process(&mut |token| {
+            println!(
+                "Token: '{}', 位置: {}, 偏移量: {}-{}",
+                token.text, token.position, token.offset_from, token.offset_to
+            );
+        });
+
+        let reader = index.reader().unwrap();
+        let searcher = reader.searcher();
+        let query_parser = tantivy::query::QueryParser::for_index(&index, vec![name]);
+        let query = query_parser.parse_query(query_text).unwrap();
+        let top_docs = searcher
+            .search(&query, &tantivy::collector::TopDocs::with_limit(10))
+            .unwrap();
+
+        println!("\n查询结果:");
+        for (score, doc_address) in top_docs {
+            let retrieved_doc: TantivyDocument = searcher.doc(doc_address).unwrap();
+            println!("文档得分: {}, 内容: {:?}", score, retrieved_doc);
+            println!("{}", retrieved_doc.to_json(&schema));
+        }
+    }
 }
